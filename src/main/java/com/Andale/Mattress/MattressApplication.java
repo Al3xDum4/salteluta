ea2af1f5c345c9c05927d6cd521e889ae5719a80
Alex Duma <alex.m.duma@gmail.com>
2020-08-04 14:34:12 +0300

    first commit

---
+++ src/main/java/com/Andale/Mattress/MattressApplication.java
@@ -0,0 +1,13 @@
+package com.Andale.Mattress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Andale/Mattress/MattressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Andale.Mattress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
